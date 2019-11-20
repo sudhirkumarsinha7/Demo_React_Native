7db6a4ef13fad0a2609f62d265864bddc8170dd9
Sudhir <ib_mac1@IB-Mac1s-MacBook-Pro.local>
2019-11-20 22:53:05 +0530

    Added increment and decrement functionality

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import {
   ScrollView,
   View,
   Text,
+  Button,
+  TouchableOpacity,
   StatusBar,
 } from 'react-native';
 
@@ -26,21 +28,43 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import Login from './Screens/login.js'
 
-const App = () => {
-  return (
-    <Login/>
-  );
-}
-export default App
-// export default class App extends Component{
-//     render(){
-//       return(
-//           <Login/>
-//         );
-//     }
-     
-   
+// const App = () => {
+//   return (
+//     <Login/>
+//   );
 // }
+// export default App
+export default class App extends Component{
+    state={
+      counter:0
+    }
+    increamentConter=()=>{
+      this.setState({counter:this.state.counter+1})
+      alert(this.state.counter)
+    }
+    decreamentCounter=()=>{
+      this.setState({counter:this.state.counter-1})
+      alert(this.state.counter)
+    }
+    render(){
+      return(
+        <View style={{flex:1}}>
+        <View style={{flex:50,justifyContent:'center',alignItems:'center'}}>
+        <TouchableOpacity onPress={()=>this.increamentConter()}>
+        <Text>Increament</Text>
+        </TouchableOpacity>
+        <Button 
+        title = 'Decrement'
+        onPress={()=>this.decreamentCounter()}>
+        </Button>
+        </View>
+        <View style={{flex:20,justifyContent:'center',alignItems:'center'}}>
+        <Text>{'Counter: '}{this.state.counter}</Text>
+        </View>
+        </View>
+        );
+    }      
+}
 
 
 
--- src/component/authentication/LoginScreen.js
+++ src/component/authentication/LoginScreen.js
@@ -1,10 +1,50 @@
 //@flow
-import type{LoginState} from '../definations/ReduxStore'
-import {connect} from 'react-redux'
-
 import React,{Component} from 'react'
 import {
         View,
-        Image,
-        StyleSheet
+        Text,
+        StyleSheet,
+        Button
 } from 'react-native'
+import { Container,Left,Right,Body,Title, Header, Content, Form, Item, Input,Footer } from 'native-base';
+import helloSaga from '../../sagas/saga'
+export default class LoginScreen extends Component<Props, State>{
+        constructor (props){
+        super(props)
+        this.state={
+                email: '',
+                password:'',
+         }
+        }
+render() {
+   return (
+      <Container>
+        <Header>
+        <Left/>
+          <Body>
+            <Title>LogIn screen</Title>
+          </Body>
+          <Right/>
+        </Header>
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
+          <Button
+          title="Login"
+          color="#f194ff"
+          onPress={() => alert('Simple Button pressed')}
+        />
+        </Content>
+        <Footer />
+      </Container>
+    );
+  
+}
+       
+}
